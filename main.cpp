492118b21cc08cc98bcfe6a93d8a3fe74a36b6d8
hathu2004 <133662222+hathu2004@users.noreply.github.com>
2023-05-22 17:04:26 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,61 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void quick_sort(int arr[],int left, int right)
+
+void sap_xep(string s)
 {
-    int i=left, j=right;
-    int pivot=arr[(left+right)/2];
-    while(i<=j)
+    vector<string>chu;
+    vector<int>so;
+    vector<string>xau;
+    stringstream ss(s);
+    string tmp;
+    while(ss>>tmp)
     {
-        while(arr[i]>pivot)
-            i++;
-        while(arr[j]<pivot)
-            j--;
-        if(i<=j)
+        xau.push_back(tmp);
+    }
+    for(string str : xau)
+    {
+        if(str[0]>='a'&&str[0]<='z'){str.pop_back();chu.push_back(str);}
+        else
         {
-            swap(arr[i],arr[j]);
-            i++;
-            j--;
+            str.pop_back();
+            int res=stoi(str);
+            so.push_back(res);
         }
     }
-    if(left<j)
-        quick_sort(arr,left,j);
-    if(i<right)
-        quick_sort(arr,i,right);
+    sort(chu.begin(),chu.end());
+    sort(so.begin(),so.end());
+    int i=0,j=0,k=0;
+    for(string str : xau)
+    {
+      if(k!=xau.size()-1)
+      {
+        if(str[0]>='a'&&str[0]<='z')
+        {
+            cout<<chu[i]<<", ";
+            i++;
+        }
+        else
+        {
+            cout<<so[j]<<", ";
+            j++;
+        }
+      }
+      else
+      {
+         if(str[0]>='a'&&str[0]<='z'){cout<<chu[i]<<".";}
+         else {cout<<so[j]<<".";}
+      }
+      k++;
+    }
 }
 int main()
 {
-    long long n,k;
-    cin>>n>>k;
-    int a[n];
-    for(int i=0;i<n;i++)
+    while(true)
     {
-        cin>>a[i];
+        string s;
+        getline(cin,s);
+        if(s=="."){break;}
+        else{sap_xep(s);cout<<endl;}
     }
-    long long sum=a[0];
-    quick_sort(a,1,n-1);
-    for(int i=1;i<=k;i++)
-    {
-       sum+=a[i];
-    }
-    for(int i=k+1;i<n;i++)
-    {
-        sum-=a[i];
-    }
-    cout<<sum;
     return 0;
 }
