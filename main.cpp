7323af9b16afed00e29172e8cd92e86b7a51656f
hathu2004 <133662222+hathu2004@users.noreply.github.com>
2024-04-10 15:34:21 +0700

    buoi7.1

--- main.cpp
+++ main.cpp
@@ -1,39 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-int binary_search(int a[],int left,int right, int value)
-{
-    int ans=0;
-    while(left<=right)
-    {
-        int middle=(right+left)/2;
-        if(a[middle]<=value){ans=middle;left=middle+1;}
-        else{right=middle-1;}
+// Hàm giải bài toán Tháp Hà Nội
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    if (n == 1) {
+        std::cout << "Di chuyen dia 1 tu cot " << from_rod << " sang cot " << to_rod << std::endl;
+        return;
     }
-    return ans;
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    std::cout << "Di chuyen dia " << n << " tu cot " << from_rod << " sang cot " << to_rod << std::endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 }
-int main()
-{
-    int n,m;
-    cin>>n>>m;
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    sort(a,a+n);
-    int result,min=a[n-1];
-    for(int i=0;i<n-2;i++)
-    {
-        for(int j=i+2;j<n;j++)
-        {
-            if(2*a[i]+a[j]<m)
-            {
-                int mid=binary_search(a,i+1,j-1,m-a[i]-a[j]);
-                if(m-a[i]-a[j]-a[mid]<min){min=m-a[i]-a[j]-a[mid];result=a[i]+a[j]+a[mid];}
-            }
-        }
-    }
-    cout<<result;
+
+int main() {
+    int n;
+    std::cout << "Nhap so dia: ";
+    std::cin >> n;
+    towerOfHanoi(n, 'A', 'C', 'B');
     return 0;
 }
